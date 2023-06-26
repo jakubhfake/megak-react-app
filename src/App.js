2bd8ef9762b738f3809fdcc7e35556d010458be7
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-26 22:46:29 +0200

    add React.Fragment

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <header className="App-header">
           <input type="text" tabIndex={-1}/>
         <p contentEditable={true}>(Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur cupiditate debitis dolores eligendi exercitationem incidunt quaerat quos unde ut! Accusamus distinctio harum laborum nobis non omnis quaerat quam recusandae sunt.</p>
       </header>
             <p>Zwracamy w React tylko jeden element</p>
-            </React.Fragment>
+            </>
   );
 }
 
