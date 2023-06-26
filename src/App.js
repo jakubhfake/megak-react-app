25950040e9438fcb64716a01bb15bf15ea8a2fab
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-26 22:33:34 +0200

    usual bugs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return; (
     <div className="App">
       <header className="App-header">
           <input type="text" tabIndex={-1}/>
