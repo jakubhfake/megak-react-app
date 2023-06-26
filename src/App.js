04cbd4619e9a28fdf8f8c8abe5846e0dbd391e8a
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-26 22:35:59 +0200

    usual bugs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return; (
+  return (
     <div className="App">
       <header className="App-header">
           <input type="text" tabIndex={-1}/>
         <p contentEditable={true}>(Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur cupiditate debitis dolores eligendi exercitationem incidunt quaerat quos unde ut! Accusamus distinctio harum laborum nobis non omnis quaerat quam recusandae sunt.</p>
       </header>
     </div>
+      <p>Zwracamy w React tylko jeden element</p>
   );
 }
 
