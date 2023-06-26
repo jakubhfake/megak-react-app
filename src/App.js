2e87cc5cb15543b4ee021f9c3f4962793c1bb5d5
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-26 22:47:40 +0200

    usual bugs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <>
+
       <header className="App-header">
           <input type="text" tabIndex={-1}/>
         <p contentEditable={true}>(Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur cupiditate debitis dolores eligendi exercitationem incidunt quaerat quos unde ut! Accusamus distinctio harum laborum nobis non omnis quaerat quam recusandae sunt.</p>
       </header>
             <p>Czytaj będy React</p>
-            </>
+
   );
 }
 
