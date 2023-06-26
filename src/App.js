f65cd4523e007a83f154a726affe0a25dd38d839
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-26 22:41:19 +0200

    add React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <header className="App-header">
           <input type="text" tabIndex={-1}/>
         <p contentEditable={true}>(Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur cupiditate debitis dolores eligendi exercitationem incidunt quaerat quos unde ut! Accusamus distinctio harum laborum nobis non omnis quaerat quam recusandae sunt.</p>
       </header>
-    </div>
-      <p>Zwracamy w React tylko jeden element</p>
+            <p>Zwracamy w React tylko jeden element</p>
+            </React.Fragment>
   );
 }
 
