319454cbf91d819b5ac31006f7acb975b410c3a9
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-28 00:07:13 +0200

    Components of Virtual DOM in REACT

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import "./App";
 import {App} from "./App";
 
 
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <>
         <App/>
         <App/>
         <App/>
-    </>,
-    document.getElementById('root'),
+    </>
 );
 
