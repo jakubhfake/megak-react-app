f7b296a7421beadd25c973db7826977d1dfb848c
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-27 22:00:32 +0200

    JS expression in REACT

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function checkName(name) {
-
-    return name === 'Kuba' ? <strong>'BAN!'</strong> : name;
-}
+    const guests = ['Julia', 'Jan', 'Julian', 'Jakub', 'Joanna'];
 
 function App() {
+    const person = {
+        firstName: 'Jakub',
+        lastName:  'Haliniak',
+    }
+
   return (
     <>
       <header className="App-header">
           <input type="text" tabIndex={-1}/>
-          <p>W nawiasy klamrowe możemy dodać dowolne wyrażenie JS, które coś zwraca np. {checkName('Kuba')}, {checkName(<h1>xss injection, </h1>)}.</p>
+          <p className={person.firstName}>W nawiasy klamrowe możemy dodać dowolne wyrażenie JS, które coś zwraca np. Witamy rodzinę {person.lastName}: {guests.join(', ')}</p>
       </header>
 
     </>
