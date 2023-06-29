cf16b1e45cc469c35de5f654461945c68f0656e6
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-29 23:07:53 +0200

    Class component in REACT

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
     const guests = ['Julia', 'Jan', 'Julian', 'Jakub', 'Joanna'];
 
-export const App = () => {
-    const person = {
-        firstName: 'Jakub',
-        lastName:  'Haliniak',
+export class App extends Component {
+    render() {
+        return (
+            <>
+                <header className="App-header">
+                    <h1>Hello, World!</h1>
+                    <p>Hello from {guests.join(', ')}.</p>
+                </header>
+            </>
+        );
     }
-
-  return (
-    <>
-      <header className="App-header">
-          <input type="text" tabIndex={-1}/>
-          <p className={person.firstName}>W nawiasy klamrowe możemy dodać dowolne wyrażenie JS, które coś zwraca np. Witamy rodzinę {person.lastName}: {guests.join(', ')}</p>
-      </header>
-
-    </>
-  );
-};
+}
 
