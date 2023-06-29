505680777e17745e2b630b903337e715bf76a5d5
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-29 23:09:37 +0200

    Function component in REACT

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import './App.css';
 
     const guests = ['Julia', 'Jan', 'Julian', 'Jakub', 'Joanna'];
 
-export class App extends Component {
-    render() {
-        return (
-            <>
-                <header className="App-header">
-                    <h1>Hello, World!</h1>
-                    <p>Hello from {guests.join(', ')}.</p>
-                </header>
-            </>
-        );
-    }
+export const App = () => {
+    return (
+        <>
+            <header className="App-header">
+                <h1>Hello, World!</h1>
+                <p>Hello from {guests.join(', ')}.</p>
+            </header>
+        </>
+    );
 }
 
