42435bade593065e98cf15f62553516d86b7ecf5
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-29 23:52:34 +0200

    Using props in REACT

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
     const guests = ['Julia', 'Jan', 'Julian', 'Jakub', 'Joanna'];
 
 export const App = (props) => {
+    const {name} = props
     return (
         <>
             <h1>Hello, World!</h1>
-            <p>Hello from {props.name}.</p>
+            <p>Hello from {name}.</p>
         </>
     );
 }
