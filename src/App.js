0c151316df0c3b0bd07e66e6262d810c2b96cb82
Jakub Haliniak <jakubh.fake@gmail.com>
2023-06-30 23:28:09 +0200

    Using props in REACT

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 
-    const guests = ['Julia', 'Jan', 'Julian', 'Jakub', 'Joanna'];
-
 export const App = (props) => {
-    const {name, lastName} = props
+    const {name, lastName, friends} = props
     return (
         <>
             <h1>Hello, World!</h1>
-            <p>Hello from {name} {lastName}.</p>
+            <p>Hello from {name} {lastName} do you have a friends:  {friends}</p>
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import {App} from "./App";
 import {render} from 'react-dom';
 
+const name = 'Baltazar';
+const friends = ['Jacek', 'Barbara', 'Smok Wawelski'];
+
 render(
     <>
-        <App name="Testowa Osoba" lastName="Nazwisko"/>
+        <App
+            name={name}
+            lastName="Nazwisko"
+            age={20}
+            friends={friends.join(', ')}
+        />
         <App name="Lord Vader"/>
         <App name="Jakub"/>
     </>,
