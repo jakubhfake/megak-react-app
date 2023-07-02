a40f1e8b0b07867e88ac5dd940a7ba40a44cc1c0
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-02 11:40:25 +0200

    Exercise 1 add data

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import React from 'react';
 import {Participant} from "./Participant";
 import {render} from 'react-dom';
 
+const data =[
+    {
+        name: 'Anna',
+        mail: 'xyz@xyz.xyz'
+    },
+    {
+        name: 'Jan',
+        mail: 'abc@abc.abc'
+    }
+]
 
 render(
     <React.StrictMode>
         <Participant
-            nameList={[
-                {
-                    name: 'Anna',
-                    mail: 'xyz@xyz.xyz'
-                },
-                {
-                    name: 'Jan',
-                    mail: 'abc@abc.abc'
-                }
-            ]}
+            list={data}
         />
     </React.StrictMode>,
     document.getElementById('root'),
