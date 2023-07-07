6be514da5216391950f607d1a5bdd90d5e23dee6
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-07 11:38:19 +0200

    work wiht setState

--- src/Hooks.js
+++ src/Hooks.js
@@ -2,11 +2,14 @@ import React, {useState} from 'react';
 
 export const Hooks = (props) => {
     // const [nazwa, setNazwa] = useState(domyślna wartość);
-    const [thingToDo,setThingToDo] = useState('koncentacja');
+    const [count,setCount] = useState(0);
+    setTimeout( () => {
+        setCount(5)
+    }, 5000);
 
     return <>
         <h1>Zabawa hook'ami</h1>
-        {thingToDo}
+        {count}
     </>;
 }
 
