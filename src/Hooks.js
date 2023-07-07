ab6f346cabfcafdd5b0f3a7d6c0f8248b26f3d14
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-07 16:03:49 +0200

    work wiht setState

--- src/Hooks.js
+++ src/Hooks.js
@@ -4,9 +4,8 @@ export const Hooks = (props) => {
     // const [nazwa, setNazwa] = useState(domyślna wartość);
     const [count,setCount] = useState(0);
     setInterval( () => {
-        setCount((count) => {
-            return count + 1;
-        })
+        setCount(count => count + 1);
+
     }, 1000);
 
     return <h1>{count}</h1>;
