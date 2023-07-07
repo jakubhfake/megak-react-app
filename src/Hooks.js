4af8c011ae8d5726fd55b0b1daafaf794c041c36
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-07 17:28:41 +0200

    work wiht setState function component convert to class

--- src/Hooks.js
+++ src/Hooks.js
@@ -3,21 +3,12 @@ import React, {useState} from 'react';
 export const Hooks = (props) => {
     // const [nazwa, setNazwa] = useState(domyślna wartość);
 
-    const [person, setPerson] = useState({
-        name: 'Ciekawe jakieś imię',
-        lastName: 'Kowalski jak zwykle',
-    });
+    const [name, setName] = useState('Foo');
 
     setTimeout( () => {
-        setPerson(person => {
-            const personCopy = JSON.parse(JSON.stringify(person));
-            personCopy.name = 'Jakub';
-            return personCopy;
-        //     jest to zmiana obiektu przez referencje, a ny nie możemy zmieniać stanu wyjściowego!
-        })
-    }, 2500);
+        setName('Bar')}, 2500);
 
 
-    return <h1>{person.name}</h1>;
+    return <h1>{name}</h1>;
 }
 
