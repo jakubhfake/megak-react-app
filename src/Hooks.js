1b1c6dcdeca51377fdaed0c83b611308c0207fed
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-07 16:03:11 +0200

    work wiht setState

--- src/Hooks.js
+++ src/Hooks.js
@@ -3,12 +3,12 @@ import React, {useState} from 'react';
 export const Hooks = (props) => {
     // const [nazwa, setNazwa] = useState(domyślna wartość);
     const [count,setCount] = useState(0);
-    setTimeout( () => {
-        setCount(5)
-    }, 5000);
+    setInterval( () => {
+        setCount((count) => {
+            return count + 1;
+        })
+    }, 1000);
 
-    return count >= 7 ?
-        <p>Poprawne jest to że nasza liczba {count} jest >= 4</p>
-        : <h1>{count} jest większe lub równe 4, sprawdź czy mamy rację!</h1>;
+    return <h1>{count}</h1>;
 }
 
