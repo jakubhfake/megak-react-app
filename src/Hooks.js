c30506d39c563385ac2d981f783377eec3cd2b20
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-20 06:07:05 +0200

    counter jest liczony co dwa

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 
 export class Hooks extends Component {
     // Metody cyklu życia komponentu
@@ -6,26 +6,29 @@ export class Hooks extends Component {
         count: 0,
     };
 
-    componentDidMount() {
-        console.log('Pierwsza metoda, zamontowanie komponentu w DOM');
+    componentDidMount(){
+        // counter jest liczony co dwa i jak narazie nie wiem o co chodzi, gdzie jest błąd
+        const {count} = this.state;
+        console.log(`Pierwsza metoda, zamontowanie komponentu w DOM ${count}`);
 
         setInterval(() => {
             this.setState(prev => ({
                 count: prev.count + 1,
-            }))
+            }));
         }, 1000);
     }
 
     componentDidUpdate() {
-        console.log('Aktualizacja ...');
+        console.log(`Aktualizacja ... ${this.state.count}`);
     }
 
+
     render() {
-        const {name} = this.state;
+        const {count} = this.state;
 
-        console.log('Renderowanie...');
+        console.log(`Renderowanie... ${count}`);
 
-        return <h1>{name}</h1>;
+        return <h1>{count}</h1>;
     }
 }
 
