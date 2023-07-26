04ddab7acf77a26380e1d1468cc573f4459fa05a
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-27 00:01:08 +0200

    use html attributes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import {CrazyInput} from "./CrazyInput";
 
 export const App = props => (
     <>
         <input type={'number'} min={0} max={100} required/>
         <input type={'text'}/>
         <div>Lalaaaallaaa aaaa!</div>
+        <CrazyInput type={'text'}/>
+        <CrazyInput type={'password'}/>
     </>
 );
 
---
+++ src/CrazyInput.js
@@ -0,0 +1,4 @@
+import React from "react";
+export const CrazyInput = props => {
+    return <input type={props.type}/>
+}
