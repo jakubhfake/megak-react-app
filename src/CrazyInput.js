542b5384cec75da5c993e27d887edb6fc2494ea6
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-27 00:08:05 +0200

    use html attributes

--- src/CrazyInput.js
+++ src/CrazyInput.js
@@ -1,4 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 export const CrazyInput = props => {
+
+    const [inputType, setInputType] = useState('password');
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    //     tu musi być funkcja a nie return clearInterval(intervalId), ponieważ po uruchomienufunkcji id razu się ona zakończy, czyli nie będzie działało :)
+    }, []);
+
     return <input type={props.isPwd ? 'password' : 'text'}/>
 }
