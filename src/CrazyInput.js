488e3ec5eedcaae333089b0d140172d7b1e31dbd
Jakub Haliniak <jakubh.fake@gmail.com>
2023-07-27 00:42:09 +0200

    use html attributes

--- src/CrazyInput.js
+++ src/CrazyInput.js
@@ -1,16 +1,17 @@
 import React, {useEffect, useState} from "react";
 export const CrazyInput = props => {
 
-    const [inputType, setInputType] = useState('password');
+    const [backgroundColor, setBackgroundColor] = useState('yellow');
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setInputType(inputType => (inputType === 'password' ? 'text' : 'password'))
+            setBackgroundColor(inputType => (backgroundColor === 'yellow' ? 'cadetblue' : 'yellow'))
+            console.log('color is changing');
         }, 1000);
 
         return () => clearInterval(intervalId);
     //     tu musi być funkcja a nie return clearInterval(intervalId), ponieważ po uruchomienufunkcji id razu się ona zakończy, czyli nie będzie działało :)
-    }, []);
+    }, [backgroundColor]);
 
-    return <input type={inputType}/>
+    return <input type={"password"} style={{backgroundColor}}/>
 }
