35ab33b47c174e006a44dd77e10e2ffb752665ee
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-02 22:49:58 +0200

    modify password hide component

--- src/PassTextInput.js
+++ src/PassTextInput.js
@@ -2,20 +2,18 @@ import React, {useState} from "react";
 
 export const PassTextInput = prompt => {
     const [inputType, setInputType] = useState('password');
-
-    const [icon, setIcon] = useState('👁‍🗨');
     const showPassword = () => {
         setInputType('text');
-        setIcon('👁‍🗨');
     };
 
     const hidePassword = () => {
         setInputType('password');
-        setIcon('👁')
     };
 
     return <>
         <input type={inputType}/>
-        <button onMouseDown={showPassword} onMouseUp={hidePassword}>{icon}</button>
+        <button onMouseDown={showPassword} onMouseUp={hidePassword}>
+            {inputType === 'password' ? '👁' : '👁‍🗨'}
+        </button>
         </>
 }
