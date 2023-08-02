83dd89c389567f5e59af0434106407687e2b6945
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-03 00:07:05 +0200

    refactoring led switching lights

--- src/LedLights.js
+++ src/LedLights.js
@@ -4,19 +4,15 @@ import './LedLights.css';
 export const LedLights = props => {
     const [color, setColor] = useState('white');
 
-    const setRed = () => setColor('red');
-    const setGreen = () => setColor('green');
-    const setBlue = () => setColor('blue');
-
     return <>
         <div className={'LedLights'}>
             <div
                 className={'LedLights__box'}
                 style={{backgroundColor: color}}
             />
-            <button onClick={setRed}>Red</button>
-            <button onClick={setGreen}>Green</button>
-            <button onClick={setBlue}>Blue</button>
+            <button onClick={() => setColor('red')}>Red</button>
+            <button onClick={() => setColor('green')}>Green</button>
+            <button onClick={() => setColor('blue')}>Blue</button>
         </div>
     </>;
 
