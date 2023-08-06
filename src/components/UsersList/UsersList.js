130ef7ba15f5750544b61b1dd429211027ad5424
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-06 22:10:26 +0200

    add basic users list view

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export const UsersList = props =>{
-
+    console.log(props.users);
+    return <ul>
+        {
+            props.users.map(
+                user => <li key={user.username}>
+                    {user.name} {user.last_name}  {user.username}
+            </li>
+            )
+        }
+    </ul>
 }
