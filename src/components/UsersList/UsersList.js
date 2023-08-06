e0826a055411430b9455ad5c35074493c7209c50
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-06 22:14:16 +0200

    refactoring

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-export const UsersList = props =>{
-    console.log(props.users);
-    const lis =  props.users
-        .map(
-        user => <li key={user.username}>
-            {user.name} {user.last_name}  {user.username}
-        </li>
-    );
-
-    return <ul>
-        {lis}
-    </ul>;
-};
+export const UsersList = props => (
+    <ul>
+        {
+            props.users.map(
+                user => <li key={user.username}>
+                    {user.name} {user.last_name}  {user.username}
+            </li>
+            )
+        }
+    </ul>
+)
