02bc84f16b55669dbe9b52b9366988671e78e00b
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-08 08:04:22 +0200

    add css for user photo

--- src/components/UsersList/UserListOneItem.js
+++ src/components/UsersList/UserListOneItem.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import './UserListOneItem.css';
+
 export const UserListOneItem = props => (
-    <li key={props.user.username}>
+    <li key={props.user.username} className={'UserListOneItem'}>
         <img
+            className={'UserListOneItem__photo'}
             src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`} alt={`Photo of ${props.user.first_name}`}
         />
         {props.user.name} {props.user.last_name}     {props.user.username}
