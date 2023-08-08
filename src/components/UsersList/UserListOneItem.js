ef147a01201e0cd750f46e0ba8aa1cbfebd78632
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-08 05:37:08 +0200

    add photo to user

--- src/components/UsersList/UserListOneItem.js
+++ src/components/UsersList/UserListOneItem.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export const UserListOneItem = props => (
     <li key={props.user.username}>
+        <img
+            src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`} alt={`Photo of ${props.user.first_name}`}
+        />
         {props.user.name} {props.user.last_name}     {props.user.username}
     </li>
 );
