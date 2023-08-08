f44c735027bbc5ee6cc0ead68626a6ba32da74e0
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-08 08:10:16 +0200

    add sort to users list

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -4,7 +4,9 @@ import {UserListOneItem} from './UserListOneItem';
 export const UsersList = props => (
     <ul>
         {
-            props.users.map(userObj => <UserListOneItem user={userObj} key={userObj.username}/>)
+            props.users
+                .sort((a, b) => a.username.localeCompare(b.username))
+                .map(userObj => <UserListOneItem user={userObj} key={userObj.username}/>)
         }
     </ul>
 )
