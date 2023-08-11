1c9394081c3a457da8679268c6dcc8888f20a6c4
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-11 05:59:34 +0200

    add alphabetical sorting user list

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import {UserListOneItem} from './UserListOneItem';
 
 import './UsersList.css';
-export const UsersList = props => (
-    <ul className={'UsersList'}>
+export const UsersList = props => {
+
+    return <ul className={'UsersList'}>
         {
-            props.users
+            [...props.users]
                 .sort((a, b) => a.username.localeCompare(b.username))
                 .map(userObj => <UserListOneItem user={userObj} key={userObj.username}/>)
         }
     </ul>
-)
+};
