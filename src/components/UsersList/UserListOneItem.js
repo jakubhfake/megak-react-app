2c50364a30af11d656e8a4fd0397715b4b987349
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-11 08:11:25 +0200

    add delete user from list

--- src/components/UsersList/UserListOneItem.js
+++ src/components/UsersList/UserListOneItem.js
@@ -2,16 +2,23 @@ import React from "react";
 
 import './UserListOneItem.css';
 
-export const UserListOneItem = props => (
-    <li key={props.user.username} className={'UserListOneItem'}>
-        <img
-            className={'UserListOneItem__photo'}
-            src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`} alt={`Photo of ${props.user.first_name}`}
-        />
-        <div className={'UserListOneItem__text'}>
-            <p>{props.user.first_name} {props.user.last_name}</p>
-            <p className={'UserListOneItem__username'}>{props.user.username}</p>
-        </div>
+export const UserListOneItem = props => {
+    const personClicked = () => {
+        props.onRemoveItem(props.user.username);
+    };
 
-    </li>
-);
+    return (
+        <li key={props.user.username} className={'UserListOneItem'} onClick={personClicked}>
+            <img
+                className={'UserListOneItem__photo'}
+                src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`}
+                alt={`Photo of ${props.user.first_name}`}
+            />
+            <div className={'UserListOneItem__text'}>
+                <p>{props.user.first_name} {props.user.last_name}</p>
+                <p className={'UserListOneItem__username'}>{props.user.username}</p>
+            </div>
+
+        </li>
+    );
+}
--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -3,12 +3,25 @@ import {UserListOneItem} from './UserListOneItem';
 
 import './UsersList.css';
 export const UsersList = props => {
+    const [list, setList] = useState(props.users);
+    const removeItem = (username) => {
+        setList( list =>(
+            list
+                .filter(oneUser => oneUser.username !== username)
+        ));
+    };
 
-    return <ul className={'UsersList'}>
-        {
-            [...props.users]
-                .sort((a, b) => a.username.localeCompare(b.username))
-                .map(userObj => <UserListOneItem user={userObj} key={userObj.username}/>)
-        }
-    </ul>
+    return (
+        <ul className={'UsersList'}>
+            {
+                [...list]
+                    .sort((a, b) => a.username.localeCompare(b.username))
+                    .map(userObj => <UserListOneItem
+                        user={userObj}
+                        key={userObj.username}
+                        onRemoveItem={removeItem}
+                    />)
+            }
+        </ul>
+    )
 };
