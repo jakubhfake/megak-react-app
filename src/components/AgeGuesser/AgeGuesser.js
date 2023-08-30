ea4b79345774cd35ebc54a84a16a9ae41744a9bf
Jakub Haliniak <jakubh.fake@gmail.com>
2023-08-30 23:12:08 +0200

    loading setup

--- src/components/AgeGuesser/AgeGuesser.js
+++ src/components/AgeGuesser/AgeGuesser.js
@@ -3,6 +3,10 @@ import React, {useState} from "react";
 export const AgeGuesser = prompt => {
     const [age, setAge] = useState(null);
 
+    if (age === null) {
+        return null;
+    }
+
     return <>
         <p>Twój wiek: {age}</p>
         <p>Twoja data urodzenia: {new Date().getFullYear() - age}</p>
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Dialog} from "../common/Dialog/Dialog";
-import {ConfirmDialog} from "../common/ConfirmDialog/ConfirmDialog";
-import {Button} from "../common/Button/Button";
-import {BigButton} from "../common/BigButton";
+
 import {AgeGuesser} from "../AgeGuesser/AgeGuesser";
 
 export const App = props => {
