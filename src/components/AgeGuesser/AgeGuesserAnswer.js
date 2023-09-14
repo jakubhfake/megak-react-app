2645d9232452af9fa3dd5968ee7f281870f749d4
Jakub Haliniak <jakubh.fake@gmail.com>
2023-09-14 21:56:23 +0200

    code without errorhandler

--- src/components/AgeGuesser/AgeGuesserAnswer.js
+++ src/components/AgeGuesser/AgeGuesserAnswer.js
@@ -2,38 +2,21 @@ import React, {useEffect, useState, useCallback} from "react";
 
 export const AgeGuesserAnswer = props => {
     const [age, setAge] = useState(null);
-    const [error, setError] = useState(null)
 
     // ToDo check error handling
 
     const fetchData = useCallback(async () => {
-                
-        try {
-            setError(null);
+
             const res = await fetch(`https://api.agify.io/?name=${props.name}`);
             console.log('Response', res);
-            if (res.ok === true) {
                 const data = await res.json();
-                // console.log(data);
                 setAge(data.age);
-                console.log('setAge', setAge(data.age), typeof data.age);
-            }
-        }
-        // catch nie wykrywa błędu, dokończyć waliację
-        catch (err) {
-            setError('Błąd wczytywania!');
 
-        }
     }, []);
 
     useEffect( () => {
         fetchData();
     }, []);
-
-    console.log('Error', error);
-    if (error) {
-        return <p>{error}</p>;
-    }
 
     if (age === null) {
         return <p> Wczytywanie....</p>;
