078b66fa093526e63f4adf61b05c727912d6c1d9
Jakub Haliniak <jakubh.fake@gmail.com>
2023-09-14 21:51:24 +0200

    add button type

--- src/components/AgeGuesser/AgeGuesser.js
+++ src/components/AgeGuesser/AgeGuesser.js
@@ -2,6 +2,27 @@ import React, {useEffect, useState, useCallback} from "react";
 import {AgeGuesserAnswer} from "./AgeGuesserAnswer";
 
 export const AgeGuesser = props => {
+    const [name, setName] = useState('');
+    const [check, setCheck] = useState(false);
 
-    return <AgeGuesserAnswer name={"Wojciech"}/>
+    const sendForm = e => {
+        e.preventDefault();
+        setCheck(true);
+    }
+
+    if (check) {
+        return <AgeGuesserAnswer name={name}/>
+    }
+
+    return <form onSubmit={sendForm}>
+        <label>
+            Podaj imię: <br/>
+            <input
+                type={"text"}
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            <button type={"submit"}>Zgadnij mój wiek!</button>
+        </label>
+    </form>
 };
